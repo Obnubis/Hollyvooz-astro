26a9a8469cc8a5a77997590032b4453573ac49b6
Obnubis <Marioroblespad@gmail.com>
2024-06-30 13:41:24 +0200

    añadida tipografía y algunos colores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			
+			colors: {
+				'principal' : '#58bbac',
+				'secundario1' : '#3b857a',
+				'secundario2' : '#91cec5',
+				'adicional1' : '#e5edef',
+				'adicional2' : '#132428',
+			},
+			fontFamily: {
+				'gotham' : ['"Gotham Book"', 'sans-serif']
+			}
 		},
 	},
 	plugins: [],
